b9c5536b90eb29d302bbc9f77ddaddae3d7347d9
Nishant Mehta <nishme0708@gmail.com>
2023-04-10 13:13:22 +0530

    Add code changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,35 @@
+import { useState, useEffect } from 'react';
+import JobInfo from './JobInfo';
+import BtnContainer from './BtnContainer';
+
 const url = 'https://course-api.com/react-tabs-project';
 
 const App = () => {
-  return <h2>Tabs Starter</h2>;
+  const [worker, setWorker] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [activeJobId,setActiveJobId] = useState(0);
+  const fetchData = async () => {
+    let resp = await fetch(url);
+    let data = await resp.json();
+    setWorker(data);
+    setLoading(false);
+  };
+  useEffect(() => {
+    setLoading(true);
+    fetchData();
+  }, []);
+  if (loading) {
+    return (
+      <section className='jobs-center'>
+        <div className='loading'></div>
+      </section>
+    );
+  }
+  return (
+    <section className='jobs-center'>
+      <BtnContainer jobs={worker} activeJobId={activeJobId} updateActiveJobId={setActiveJobId}></BtnContainer>
+      <JobInfo job={worker[activeJobId]}></JobInfo>
+    </section>
+  );
 };
 export default App;
